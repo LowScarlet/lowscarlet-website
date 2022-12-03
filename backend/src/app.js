75dfa70f85b0cf2221da1ba786a9dd50e7c10ebb
Low_Scarlet <namaetohyaakiradesu@gmail.com>
2022-12-04 05:19:18 +0700

    !

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.get('/', (req, res) => {
   res.json({
